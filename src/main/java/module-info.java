3cc4a1f5d5236d4336d558f64a8dd764222671f5
CinnamonShake45 <shikhardixit09@gmail.com>
2024-12-29 18:17:51 +0530

    exported my own modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,11 @@
 module com.cinnamonshake {
-    requires javafx.controls;
+    requires transitive javafx.controls;
     requires javafx.fxml;
     requires java.sql;
     requires javafx.web;
 
     opens com.cinnamonshake to javafx.fxml;
     exports com.cinnamonshake;
+    exports com.cinnamonshake.services;
+    exports com.cinnamonshake.models;
 }
