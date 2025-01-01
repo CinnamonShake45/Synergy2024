02b7ffb84a267e694aed327f88c2630a371c5b89
CinnamonShake45 <shikhardixit09@gmail.com>
2025-01-01 16:27:24 +0530

    removed unneccessary statements

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,7 @@
+// nothing needs to be exported or opened, as of now
 module com.cinnamonshake {
-    requires transitive javafx.controls;
+    requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
     requires javafx.web;
-
-    opens com.cinnamonshake to javafx.fxml;
-    exports com.cinnamonshake;
-    exports com.cinnamonshake.services;
-    exports com.cinnamonshake.models;
 }
