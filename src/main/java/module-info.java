b46e2a4d0620c46bf073ac306d799504db47a443
CinnamonShake45 <shikhardixit09@gmail.com>
2025-01-01 17:51:50 +0530

    rewrote properly (i think)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,12 @@
 // nothing needs to be exported or opened, as of now
 module com.cinnamonshake {
-    requires javafx.controls;
+    requires transitive javafx.controls;
     requires javafx.fxml;
     requires java.sql;
     requires javafx.web;
+
+    opens com.cinnamonshake to javafx.graphics, javafx.fxml, javafx.web;
+    exports com.cinnamonshake;
+    exports com.cinnamonshake.models;
+    exports com.cinnamonshake.services;
 }
